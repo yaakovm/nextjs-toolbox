d3689793a23acc2cb8a5211a59763102cef5d416
yaakovm <yaakovmoddel@gmail.com>
2022-11-02 23:35:55 -0400

    url fix

--- netlify/functions/joke.js
+++ netlify/functions/joke.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 export const handler = async (event) => {
 
-  const result = await fetch('/random.json?language=en');
+  const result = await fetch('https://uselessfacts.jsph.pl/random.json?language=en');
   const data = await result.json();
      return {
         statusCode: 200,
