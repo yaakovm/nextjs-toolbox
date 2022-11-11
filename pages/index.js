a7c9f6f0738e83c128ad8984850e4209a0d151e1
yaakovm <yaakovmoddel@gmail.com>
2022-11-11 11:28:07 +0000

    title and favicon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>The Boogiewoogie site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
